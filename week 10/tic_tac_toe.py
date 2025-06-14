ab1cee30bd2b626927fbee4e16dd3ce92c7844fb
Biplab1992 <neubiplab@gmail.com>
2025-06-15 09:55:58 +1200

    a

--- week 10/tic_tac_toe.py
+++ week 10/tic_tac_toe.py
@@ -1,11 +1,11 @@
 # prints a simple tic tac toe game in the console
 def print_board(board):
     print("\n")
-    print(" {} | {} | {} ".format(board[0], board[1], board[2]))
+    print(f" {board[0]} | {board[1]} | {board[2]} ")
     print("---+---+---")
-    print(" {} | {} | {} ".format(board[3], board[4], board[5]))
+    print(f" {board[3]} | {board[4]} | {board[5]} ")
     print("---+---+---")
-    print(" {} | {} | {} ".format(board[6], board[7], board[8]))
+    print(f" {board[6]} | {board[7]} | {board[8]} ")
     print("\n")
 
 # A function to check if a player has won the game
